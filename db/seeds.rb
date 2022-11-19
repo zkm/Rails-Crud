10638674cf5905cf0af26e869ffdbae8e9d11068
Zach Schneider <me@zachschneider.com>
2022-11-19 10:59:27 -0600

    basic crud using rails

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Dog.create(name: 'Hendrix', motto: 'Squirrel!')
+Dog.create(name: 'Nala', motto: 'Born to be wild')
+Dog.create(name: 'Alex', motto: 'Calm as can be')
+Dog.create(name: 'Leroy', motto: 'Life of the party')
+Dog.create(name: 'Belle', motto: 'Miss Independent')
